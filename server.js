10b93a741b0dd7022f545f5c89ba332f77d416e3
Fenyyx <fenixg12@gmail.com>
2025-03-17 15:06:52 +0000

    prueba2

--- server.js
+++ server.js
@@ -28,14 +28,9 @@ const PORT = process.env.PORT || 8000
 
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors(*));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://xcom3.netlify.app");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-  });
+
   
 
 app.use(express.json())
