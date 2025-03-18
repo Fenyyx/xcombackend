cc9339c87402a4b0f6881fe89b331a6c77eec5d8
Fenyyx <fenixg12@gmail.com>
2025-03-18 16:00:51 +0000

    edhnbn

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -92,7 +92,6 @@ export const logout = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production", // Solo 'true' en producción
             sameSite: "strict",
-            path: "/",
         });
         res.status(200).json({message:"Logged out succesfully"})
     } catch (error) {
